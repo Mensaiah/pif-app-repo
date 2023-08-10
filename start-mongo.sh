7943f5cbbcb6180700d7581bee17c13b5cc40288
Abdulsamii Ajala <piftech@pif-app.com>
2023-08-10 10:34:29 +0100

    fix: start-mongo shell script

--- start-mongo.sh
+++ start-mongo.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-# Function to check if a process is running
-is_process_running() {
-    pgrep $1 >/dev/null 2>&1
+# Function to check if a process is running on a specific port
+is_process_running_on_port() {
+    lsof -i :$1 | grep LISTEN >/dev/null 2>&1
 }
 
 # Check if MongoDB processes are already running
-if is_process_running mongod; then
-    echo "MongoDB instances are already running."
-else
+if is_process_running_on_port 27017 && ! is_process_running_on_port 27018 && ! is_process_running_on_port 27019; then
+    echo "Only MongoDB instance on port 27017 is running. Killing it."
+    # Kill the MongoDB process running on port 27017
+    pkill -f "mongod --port 27017"
+    sleep 2 # Wait for a couple of seconds to ensure the process is killed
+fi
+
+# Start the MongoDB instances if they aren't running
+if ! is_process_running_on_port 27017 || ! is_process_running_on_port 27018 || ! is_process_running_on_port 27019; then
     # Step 1: Create data directories
     mkdir -p local-db/rs1 local-db/rs2 local-db/rs3
 
     # Start MongoDB instances as background processes
-
-mongod --port 27017 --dbpath local-db/rs1 --replSet pif-repl --logpath local-db/rs1.log --fork --bind_ip 127.0.0.1
-mongod --port 27018 --dbpath local-db/rs2 --replSet pif-repl --logpath local-db/rs2.log --fork --bind_ip 127.0.0.1
-mongod --port 27019 --dbpath local-db/rs3 --replSet pif-repl --logpath local-db/rs3.log --fork --bind_ip 127.0.0.1
-
+    mongod --port 27017 --dbpath local-db/rs1 --replSet pif-repl --logpath local-db/rs1.log --fork --bind_ip 127.0.0.1
+    mongod --port 27018 --dbpath local-db/rs2 --replSet pif-repl --logpath local-db/rs2.log --fork --bind_ip 127.0.0.1
+    mongod --port 27019 --dbpath local-db/rs3 --replSet pif-repl --logpath local-db/rs3.log --fork --bind_ip 127.0.0.1
 
     # Wait for MongoDB instances to start
     sleep 6
@@ -36,4 +40,6 @@ mongod --port 27019 --dbpath local-db/rs3 --replSet pif-repl --logpath local-db/
     rs.initiate(rsconf)
     rs.status()
 EOF
+else
+    echo "MongoDB instances are already running."
 fi
